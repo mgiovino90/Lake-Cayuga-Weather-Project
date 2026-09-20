658af234a0836a8a706bdfc34191de6372aab851
Marissa <marissa.giovino@gmail.com>
2026-09-19 20:36:22 -0400

    fixed it so it runs every hour

--- upload_data_to_bigquery_v1.py
+++ upload_data_to_bigquery_v1.py
@@ -86,7 +86,9 @@ def upload_data(lat, long, api, table):
 if __name__=="__main__":
 
     lat_list, long_list= read_coords(file= os.getenv(key="coord_json"))
-    for i, j in enumerate(tables):
-        upload_data(lat= lat_list[i], long= long_list[i], api= api, table= j)
 
-    time.sleep(3600)
+    while True:
+        for i, j in enumerate(tables):
+            upload_data(lat= lat_list[i], long= long_list[i], api= api, table= j)
+
+        time.sleep(3600)
